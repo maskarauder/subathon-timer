a55984c7f57a348d0af0897d3636fae61ff34cd4
maskarauder <mask@maskarauder.com>
2025-08-27 15:52:20 -0400

    Adds logging of API calls to CSVs for subathon analytics.

--- twitch.py
+++ twitch.py
@@ -4,7 +4,7 @@
 from twitchAPI.twitch import Twitch
 from twitchAPI.oauth import CodeFlow, UserAuthenticationStorageHelper
 from twitchAPI.helper import first
-from twitchAPI.object.eventsub import ChannelCheerEvent, ChannelSubscribeEvent, ChannelSubscriptionGiftEvent, ChannelSubscriptionMessageEvent, ChannelPointsCustomRewardRedemptionAddEvent
+from twitchAPI.object.eventsub import ChannelBitsUseEvent, ChannelSubscribeEvent, ChannelSubscriptionGiftEvent, ChannelSubscriptionMessageEvent, ChannelPointsCustomRewardRedemptionAddEvent
 from twitchAPI.eventsub.websocket import EventSubWebsocket
 from uuid import UUID
 
@@ -14,16 +14,42 @@ from config import *
 from obs import *
 from threading import Thread
 
+from os import path, makedirs
+
+from csv import writer
+
 import webbrowser
 
 obs_thread = OBSThread()
 
-async def callback_bits(data: ChannelCheerEvent) -> None:
+async def write_to_logfile(file: str, message: list):
+    with open(path.join(LOG_DIRECTORY, file), 'a+') as f:
+        w = writer(f)
+        w.writerow(message)
+
+
+async def callback_bits(data: ChannelBitsUseEvent) -> None:
     global obs_thread
 
     nbits = data.event.bits
     if nbits >= TRIGGER_BITS_VALUE:
         obs_thread.update_time(nbits * BITS_VALUE)
+
+    if not LOG_ENABLED:
+        return
+    
+    user_login = data.event.user_login
+    public_name = data.event.user_name
+
+    if not user_login:
+        user_login = 'anon'
+    if not public_name:
+        public_name = 'anon'
+
+    msg = data.event.message.text
+
+    await write_to_logfile(BITS_LOGFILE, [user_login, public_name, nbits, msg, nbits * BITS_VALUE])
+    
 
 async def callback_channelpoints(data: ChannelPointsCustomRewardRedemptionAddEvent) -> None:
     global obs_thread
@@ -53,10 +79,71 @@ async def callback_new_subscriber(data: ChannelSubscribeEvent) -> None:
     tier = data.event.tier
     add_sub_time(tier)
 
+    if not LOG_ENABLED:
+        return
+    
+    login_name = data.event.user_login
+    public_name = data.event.user_name
+
+    if data.event.tier == '1000':
+        tier = '1'
+    elif data.event.tier == '2000':
+        tier = '2'
+    elif data.event.tier == '3000':
+        tier = '3'
+
+    if data.event.is_gift:
+        msg = 'gifted'
+    else:
+        msg = 'new sub'
+
+    await write_to_logfile(SUBSCRIPTION_LOGFILE, [login_name, public_name, 0, tier, msg])
+
+
 async def callback_resubscriber(data: ChannelSubscriptionMessageEvent) -> None:
-    # TODO: Maybe support multimonth subs?
     tier = data.event.tier
     add_sub_time(tier)
+
+    if not LOG_ENABLED:
+        return
+
+    login_name = data.event.user_login
+    public_name = data.event.user_name
+    value = 0
+
+    if data.event.tier == '1000':
+        tier = '1'
+        value = TIER_1_VALUE
+    elif data.event.tier == '2000':
+        tier = '2'
+        value = TIER_2_VALUE
+    elif data.event.tier == '3000':
+        tier = '3'
+        value = TIER_3_VALUE
+
+    # TODO: Maybe support multimonth subs?
+    # data.event.duration_months?
+    await write_to_logfile(SUBSCRIPTION_LOGFILE, [login_name, public_name, data.event.cumulative_months, tier, data.event.message.text, value])
+
+
+# This is to track who is gifting the subs, does not interact with the timer
+async def callback_somebody_gifted(data: ChannelSubscriptionGiftEvent) -> None:
+    if data.event.is_anonymous:
+        login_name = 'anon'
+        public_name = 'anon'
+    else:
+        login_name = data.event.user_login
+        public_name = data.event.user_name
+
+    nsubs = data.event.total
+    if data.event.tier == '1000':
+        tier = '1'
+    elif data.event.tier == '2000':
+        tier = '2'
+    elif data.event.tier == '3000':
+        tier = '3'
+    
+    await write_to_logfile(GIFT_PACKS_LOGFILE, [login_name, public_name, nsubs, tier])
 
 
 async def setup_twitch_listener():
@@ -86,7 +173,7 @@ async def setup_twitch_listener():
     obs_thread.start()
 
     if AuthScope.BITS_READ in target_scope:
-        await eventsub.listen_channel_cheer(user.id, callback_bits)
+        await eventsub.listen_channel_bits_use(user.id, callback_bits)
     if AuthScope.CHANNEL_READ_SUBSCRIPTIONS in target_scope:
         # First time subscribers and gift subs
         await eventsub.listen_channel_subscribe(user.id, callback_new_subscriber)
@@ -94,6 +181,20 @@ async def setup_twitch_listener():
         await eventsub.listen_channel_subscription_message(user.id, callback_resubscriber)
     if AuthScope.CHANNEL_READ_REDEMPTIONS in target_scope:
         await eventsub.listen_channel_points_custom_reward_redemption_add(user.id, callback_channelpoints)
+
+    if LOG_ENABLED:
+        if AuthScope.CHANNEL_READ_SUBSCRIPTIONS in target_scope:
+            await eventsub.listen_channel_subscription_gift(user.id, callback_somebody_gifted)
+
+        if not path.exists(LOG_DIRECTORY):
+            makedirs(LOG_DIRECTORY)
+
+        if not path.exists(path.join(LOG_DIRECTORY, SUBSCRIPTION_LOGFILE)):
+            await write_to_logfile(SUBSCRIPTION_LOGFILE, ['User login', 'Username', 'Streak', 'Tier', 'Resub message or source of new sub', 'Time added'])
+        if not path.exists(path.join(LOG_DIRECTORY, BITS_LOGFILE)):
+            await write_to_logfile(BITS_LOGFILE, ['User login', 'Username', 'Bits amount', 'Message', 'Time added'])
+        if not path.exists(path.join(LOG_DIRECTORY, GIFT_PACKS_LOGFILE)):
+            await write_to_logfile(GIFT_PACKS_LOGFILE, ['User login', 'Username', '# Subs', 'Tier'])
 
     running = True
     while running:
